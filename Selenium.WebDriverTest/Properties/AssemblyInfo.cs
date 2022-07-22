f873d4c4dfcac46f6e3c8c1830d1b44043d2ee45
Ahmet Ata Şentürk <atasentrk4526@gmail.com>
2022-07-22 14:44:40 +0300

    Add project files.

---
+++ Selenium.WebDriverTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Selenium.WebDriverTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Selenium.WebDriverTest")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a1626726-3327-4362-ab61-26f67960ed26")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Selenium.WebDriverTest/UnitTest1.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Interactions;
+using System;
+using System.Linq;
+using System.Threading;
+
+namespace Selenium.WebDriverTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        IWebDriver driver;
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //search - box
+            driver = new ChromeDriver();
+
+            driver.Url = "https://www.trendyol.com/";
+
+            driver.FindElement(By.ClassName(Utility.ModalClose)).Click();
+            Thread.Sleep(1000);
+            string searchValue = RandomString();
+            driver.FindElement(By.ClassName("search-box")).SendKeys(searchValue);
+            Thread.Sleep(1000);
+            driver.FindElement(By.ClassName("search-box")).SendKeys(Keys.Enter);
+            Thread.Sleep(1000);
+            Random random = new Random();
+            int randomProductNumber = random.Next(2, 5);
+            driver.FindElement(By.XPath(".//*[@id='search-app']/div/div[1]/div[2]/div[5]/div/div["+ randomProductNumber + "]/div[1]/a")).Click();
+            Thread.Sleep(1000);
+            driver.SwitchTo().Window(driver.WindowHandles.Last());
+            Thread.Sleep(1000);
+            driver.FindElement(By.ClassName("add-to-basket")).Click();
+            Thread.Sleep(2000);
+            driver.FindElement(By.ClassName("account-basket")).Click();
+            Thread.Sleep(1000);
+            driver.FindElement(By.ClassName("i-trash")).Click();
+            Thread.Sleep(1000);
+            driver.FindElement(By.XPath("//button[text()='Sil']")).Click();
+
+            //bdd
+        }
+
+        private static Random random = new Random();
+        public static string RandomString()
+        {
+            string[] strArr = { "televizyon", "yüz maskesi" };
+            return strArr[random.Next(strArr.Length)];
+        }
+    }
+}
+
+public static class Utility
+{
+    public const string ModalClose = "modal-close";
+    public const string SearchBox = "search-box";
+}
